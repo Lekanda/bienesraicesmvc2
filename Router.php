c076bc9e2129b7f9c3e3e91ff773ac6f54ae528d
lekanda <abernaolao@gmail.com>
2021-10-30 14:15:24 +0200

    Registrar urls y metodos asociados a ellas.

--- Router.php
+++ Router.php
@@ -3,8 +3,36 @@
 namespace MVC;
 
 class Router  {
-    public function __construct()
-    {
-        echo 'Creando el Router';
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    // Mete en $rutasGET los nombres de las funciones de las rutas de 'public/index.php'.
+    public function get($url,$fn){
+        // Llena el array con las funciones de las rutas que estan en el 'public/index.php'.
+        $this->rutasGET[$url] = $fn;
+        // debuguear($this->rutasGET[$url]);
+    }
+
+
+    public function comprobarRutas(){
+        // Con PATH_INFO leemos la peticion que le hacemos  al servidor (p.ej: /propiedades)
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metodo === 'GET') {
+            // Comprueba que 'rutasGET[]' y lo escrito en la url sea igual y trae la funcion asociada a $fn.
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        // Sí La funcion existe y su funcion asociada
+        if ($fn) {
+            // Llamar a una funcion cuando no se sabe como se llamara. 
+            call_user_func($fn,$this);
+        } else {
+            echo 'Pagina no encontrada';
+        }
+
+        
     }
 }
--- public/index.php
+++ public/index.php
@@ -6,4 +6,9 @@ use MVC\Router;
 
 $router = new Router();
 
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/tienda', 'funcion_tienda');
+$router->get('/menu', 'funcion_menu');
+$router->get('/admin', 'funcion_admin');
 
+$router->comprobarRutas();
