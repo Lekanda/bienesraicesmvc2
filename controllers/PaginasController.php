1f1e3404831f6273239fd94be3172b5289d1c58c
lekanda <abernaolao@gmail.com>
2021-10-31 18:13:06 +0100

    Nosotros

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -17,13 +17,23 @@ class PaginasController{
             'inicio' => $inicio
         ]);
     }
+
+
     public static function nosotros(Router $router){
 
         $router->render('paginas/nosotros', []);
     }
-    public static function propiedades(){
-        echo "Desde propiedades";
+
+
+    public static function propiedades(Router $router){
+        $propiedades = Propiedad::all();
+
+        $router->render('paginas/propiedades', [
+            'propiedades' => $propiedades
+        ]);
     }
+
+
     public static function propiedad(){
         echo "Desde propiedad";
     }
---
+++ views/paginas/propiedades.php
@@ -0,0 +1,6 @@
+<section class="seccion contenedor">
+    <h2>Casas y Fincas en Venta</h2>
+    <?php
+        include 'listado.php';
+    ?>
+</section>
