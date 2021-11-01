058dca98d7194d9998786443bfd36f1a5ab3b4a0
lekanda <abernaolao@gmail.com>
2021-11-01 15:18:11 +0100

    Proteger paginas.

--- Router.php
+++ Router.php
@@ -20,6 +20,23 @@ class Router  {
 
 
     public function comprobarRutas(){
+
+        session_start();
+
+        $auth = $_SESSION['login'] ?? null;
+        // debuguear($auth);
+
+        // Arreglo de rutas protegidas
+        $rutas_protegidas = ['/admin',
+                             '/propiedades/crear',
+                             '/propiedades/actualizar',
+                             '/propiedades/eliminar',
+                             '/vendedores/crear',
+                             '/vendedores/actualizar',
+                             '/vendedores/eliminar',
+        ];
+
+
         // Con PATH_INFO leemos la peticion que le hacemos  al servidor (p.ej: /propiedades)
         $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
@@ -31,8 +48,12 @@ class Router  {
             // Comprueba que 'rutasPOST[]' y lo escrito en la url sea igual y trae la funcion asociada a $fn.
             $fn = $this->rutasPOST[$urlActual] ?? null;
         }
-        
 
+        // Proteger las rutas
+        if (in_array($urlActual,$rutas_protegidas) && !$auth) {
+            header('Location: /');
+        }
+        
         // Sí La funcion existe y su funcion asociada
         if ($fn) {
             // Llamar a una funcion cuando no se sabe como se llamara. 
@@ -51,7 +72,6 @@ class Router  {
             // $$:Al llamar a $key devuelve el valor $value
             $$key = $value;
         }
-
 
         ob_start(); // Inicia un almacenamiento en memoria.
         include __DIR__ . "/views/$view.php";
