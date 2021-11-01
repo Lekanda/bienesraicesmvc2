36f84ebc199954952a6d2c601aebf2ba191be692
lekanda <abernaolao@gmail.com>
2021-11-01 10:22:23 +0100

    Configuracion para mandar email con datos de contacto a mailtrap.

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class PaginasController{
 
@@ -57,8 +58,54 @@ class PaginasController{
     public static function contacto(Router $router){
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            debuguear($_POST);
+            $respuestas = $_POST['contacto'];
+
+            // Crear una instancia de PHPMailer
+            $mail = new PHPMailer();
+
+            // Configurar SMTP
+            $mail->isSMTP();
+            $mail->Host = 'smtp.mailtrap.io';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = '0aa2429c90a0f5';
+            $mail->Password  =  '3f8880e6995f75';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port       = 2525;  
+
+            // Configurar el contenido del mail
+            $mail->setFrom('admin@bienesraices.com'); // Quien envia el eMail
+            $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com'); // A que email llega el correo
+            $mail->Subject = 'Tienes un nuevo mensaje'; // Asunto del mail
+
+            //Habilitar
+            $mail->isHTML(true);
+            $mail->CharSet = 'UTF-8';
+
+            // Definir el contenido
+            $contenido = '<html>';
+            $contenido .= '<p>Tienes un nuevo mensaje</p>';
+            $contenido .= '<p>Nombre: ' . $respuestas['nombre']  . '</p>';
+            $contenido .= '<p>Email: ' . $respuestas['email']  . '</p>';
+            $contenido .= '<p>Telefono: ' . $respuestas['telefono']  . '</p>';
+            $contenido .= '<p>Mensaje: ' . $respuestas['mensaje']  . '</p>';
+            $contenido .= '<p>Venta o compra: ' . $respuestas['tipo']  . '</p>';
+            $contenido .= '<p>Presupuesto:  €' . $respuestas['precio']  . '</p>';
+            $contenido .= '<p>Tipo Contacto: ' . $respuestas['contacto']  . '</p>';
+            $contenido .= '<p>Fecha: ' . $respuestas['fecha']  . '</p>';
+            $contenido .= '<p>Hora: ' . $respuestas['hora']  . '</p>';
+            $contenido .= '</html>';
+
+            $mail->Body = $contenido;
+            $mail->AltBody = 'Esto es txt alternativo sin HTML';
+
+            // Enviar el mail
+            if($mail->send()){
+                echo 'Mensaje enviado correctamente';
+            } else {
+                echo 'Mensaje NO enviado';
+            }
         }
+
 
 
         $router->render('paginas/contacto', []);
