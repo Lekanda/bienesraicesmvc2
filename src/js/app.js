b914a27155a2ce38a216bb522342473540842e13
lekanda <abernaolao@gmail.com>
2021-11-01 11:43:14 +0100

    Poner un listener a opciones de contacto en formulario de contacto. Seleccionar un metodo  de contacto.

--- src/js/app.js
+++ src/js/app.js
@@ -37,6 +37,15 @@ function eventListeners() {
     mobileMenu.addEventListener('click', navegacionResponsive);
 
 
+    /***********************************************************************************/
+    // Muestra campos del formulario de forma condicional
+    const metodoContacto = document.querySelectorAll('input[name="contacto[contacto]"]');
+    // Itera en el select del formulario y pone un eventlistener a cada uno
+    metodoContacto.forEach(input => input.addEventListener('click', mostrarMetodoContacto));
+    /************************************************************************************/
+
+
+
     // Eliminar texto de confirmación de CRUD en admin/index.php
     setTimeout(function(){        
         const mensajeConfirm = document.querySelector('.alerta.exito');
@@ -44,6 +53,10 @@ function eventListeners() {
         const padre = mensajeConfirm.parentElement;
         padre.removeChild(mensajeConfirm);}
     }, 3000);
+}
+
+function mostrarMetodoContacto(){
+    console.log('Seleccionando...');
 }
 
 function navegacionResponsive() {
